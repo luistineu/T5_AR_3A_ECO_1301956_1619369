55fedc916a5b315007e8dccaee732bb679b0d1ef
luistineu <luistineu@gmail.com>
2018-11-03 15:36:46 -0300

    first commit

---
+++ src/Usuario.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Usuario {
+	
+	private long id_usuario;
+	private String nome;
+	private String sobrenome;
+	private String sexo;
+	private Date nascimento;
+	private int cpf;
+	private String email;
+	
+	private String login;
+	private String senha;
+	
+	
+}
